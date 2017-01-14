140d33f22beb6c0c8176eb55a7e6d6bdcb16711c
Mayowa Adegeye <mayowa.adegeye@konga.com>
2017-01-14 17:59:10 +0100

    feat: Added touch and click listener to chat head. Created layout for mainactivity to start service when button is clicked.

--- app/src/main/java/com/mayojava/chathead/ChatHeadService.java
+++ app/src/main/java/com/mayojava/chathead/ChatHeadService.java
@@ -7,8 +7,10 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 /**
  * Created by mayowa on 14/01/2017.
@@ -36,26 +38,9 @@ public class ChatHeadService extends Service {
         mChatView = LayoutInflater.from(this)
                 .inflate(R.layout.layout_chat_head, null);
 
-        //Add the view to the window
-        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT
-        );
+        displayChatHeadOnScreenTopLeftCorner();
 
-        /**
-         * specify the chat head position. Initially view will be added to top-left corner
-         */
-
-        params.gravity = Gravity.TOP | Gravity.LEFT;
-        params.x = 0;
-        params.y = 100;
-
-        //Add the view to the window
-        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        mWindowManager.addView(mChatView, params);
+        setChatHeadCloseButton();
     }
 
     @Override
@@ -65,5 +50,92 @@ public class ChatHeadService extends Service {
         if (mChatView != null) {
             mWindowManager.removeView(mChatView);
         }
+    }
+
+    private void displayChatHeadOnScreenTopLeftCorner() {
+        //Add the view to the window
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT
+        );
+
+
+        //specify the chat head position. Initially view will be added to top-left corner
+
+        params.gravity = Gravity.TOP | Gravity.START;
+        params.x = 0;
+        params.y = 100;
+
+        //Add the view to the window
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(mChatView, params);
+
+        //add touch listener to chat head
+        dragAndMoveChatHeadOnTouch(params);
+    }
+
+    private void setChatHeadCloseButton() {
+        ImageView closeButton = (ImageView) mChatView.findViewById(R.id.close_button);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopSelf();
+            }
+        });
+    }
+
+    private void dragAndMoveChatHeadOnTouch(final WindowManager.LayoutParams params) {
+        final ImageView chatHeadImage = (ImageView) mChatView.findViewById(R.id.chat_head_profile_iv);
+
+        chatHeadImage.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+            private int lastAction;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = params.x;
+                        initialY = params.y;
+
+                        initialTouchX = motionEvent.getRawX();
+                        initialTouchY = motionEvent.getRawY();
+
+                        lastAction = motionEvent.getAction();
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        if (lastAction == MotionEvent.ACTION_DOWN) {
+                            //Open the chat conversation
+                            Intent intent = new Intent(ChatHeadService.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+
+                            //close service and remove chat head
+                            stopSelf();
+                        }
+
+                        lastAction = motionEvent.getAction();
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        //calculate the X and Y coordinates of the view
+                        params.x = initialX + (int) (motionEvent.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (motionEvent.getRawY() - initialTouchY);
+
+                        mWindowManager.updateViewLayout(mChatView, params);
+                        lastAction = motionEvent.getAction();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/mayojava/chathead/MainActivity.java
+++ app/src/main/java/com/mayojava/chathead/MainActivity.java
@@ -1,13 +1,61 @@
 package com.mayojava.chathead;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_DRAW_OVER_PERMISSION = 2084;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //check if the application has permission to draw over other apps or not
+        //This permission is granted by default for API < 23. But for API > 23
+        //you have to ask for the permission in runtime
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+            //If the draw over permission is not available, open the settings screen to grant permission
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, REQUEST_CODE_DRAW_OVER_PERMISSION);
+        } else {
+            initializeView();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_DRAW_OVER_PERMISSION) {
+
+            //check if permission was granted or not
+            if (resultCode == RESULT_OK) {
+                initializeView();
+            } else {
+                Toast.makeText(this, "Draw over other app permission not available. " +
+                        "Closing the application", Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void initializeView() {
+        findViewById(R.id.button_show_chat_head).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, ChatHeadService.class));
+                finish();
+            }
+        });
     }
 }
